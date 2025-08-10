892208104d7035c9559a842cba37c6a67f53960b
Aleksandr Pervushin <alexpervushin@icloud.com>
2025-08-10 16:39:03 +0300

    feat(): fix user model creation and update methods in services

--- app/domains/users/services.py
+++ app/domains/users/services.py
@@ -1,11 +1,7 @@
-from typing import Any, cast
+from typing import Any
 
 from advanced_alchemy.repository import SQLAlchemyAsyncRepository
-from advanced_alchemy.service import (
-    ModelDictT,
-    SQLAlchemyAsyncRepositoryService,
-    schema_dump,
-)
+from advanced_alchemy.service import SQLAlchemyAsyncRepositoryService, schema_dump
 from litestar.exceptions import NotAuthorizedException
 
 from app.core import crypt
@@ -18,22 +14,18 @@ class UserService(SQLAlchemyAsyncRepositoryService[UserModel]):
 
     repository_type = Repository
 
-    async def to_model_on_create(
-            self, data: ModelDictT[UserModel]
-    ) -> ModelDictT[UserModel]:
-        dumped: dict[str, Any] = schema_dump(data)
-        populated = await self._populate_with_hashed_password(dumped)
-        return cast("ModelDictT[UserModel]", populated)
+    async def create(self, data: Any) -> UserModel:  # type: ignore[override]
+        dumped = schema_dump(data)
+        dumped = await self._populate_with_hashed_password(dumped)
+        return await super().create(dumped)
 
-    async def to_model_on_update(
-            self, data: ModelDictT[UserModel]
-    ) -> ModelDictT[UserModel]:
-        dumped: dict[str, Any] = schema_dump(data)
-        populated = await self._populate_with_hashed_password(dumped)
-        return cast("ModelDictT[UserModel]", populated)
+    async def update(self, item_id: Any, data: Any) -> UserModel:  # type: ignore[override]
+        dumped = schema_dump(data)
+        dumped = await self._populate_with_hashed_password(dumped)
+        return await super().update(item_id=item_id, data=dumped)
 
     async def _populate_with_hashed_password(
-            self, data: dict[str, Any]
+        self, data: dict[str, Any]
     ) -> dict[str, Any]:
         if isinstance(data, dict) and "password" in data:
             password = data.pop("password")
@@ -43,7 +35,7 @@ class UserService(SQLAlchemyAsyncRepositoryService[UserModel]):
     async def authenticate(self, email: str, password: str) -> UserModel:
         db_obj = await self.get_one_or_none(email=email)
         if not db_obj or not await crypt.verify_password(
-                password, db_obj.hashed_password
+            password, db_obj.hashed_password
         ):
             msg = "Неправильный email или пароль"  # TODO: потом убрать
             raise NotAuthorizedException(detail=msg)
